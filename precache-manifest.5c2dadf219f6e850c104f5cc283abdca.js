3bf1a56f7868603d72b4f46a4454938c613a85f9
Cássio Pereira <cassiorodp@gmail.com>
2021-12-10 18:09:59 -0300

    Updates

---
+++ precache-manifest.5c2dadf219f6e850c104f5cc283abdca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9294555d1d19ba3f9aaf8f8f9d775210",
+    "url": "/star-wars-planet-search/index.html"
+  },
+  {
+    "revision": "1f78daa3f0cea50803ac",
+    "url": "/star-wars-planet-search/static/css/main.57721ec1.chunk.css"
+  },
+  {
+    "revision": "eaa0a548e38f4f59cb43",
+    "url": "/star-wars-planet-search/static/js/2.6189d943.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/star-wars-planet-search/static/js/2.6189d943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f78daa3f0cea50803ac",
+    "url": "/star-wars-planet-search/static/js/main.0b52a4b8.chunk.js"
+  },
+  {
+    "revision": "75d88ed54958424f9b9c",
+    "url": "/star-wars-planet-search/static/js/runtime-main.f989d017.js"
+  },
+  {
+    "revision": "5a6dff6593847b8c865bdda28027ccf3",
+    "url": "/star-wars-planet-search/static/media/background.5a6dff65.jpg"
+  },
+  {
+    "revision": "833dad69e51dc7e17ca1085b8835cc3d",
+    "url": "/star-wars-planet-search/static/media/pixel-gif.833dad69.gif"
+  }
+]);
